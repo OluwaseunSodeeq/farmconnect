3ba63dd5abde4699ebdfb73bba8e43d7b85d9465
OluwaseunSodeeq <ademolaoluwaseun90@gmail.com>
2025-10-17 17:41:21 +0100

    few pages added

--- app/dashboardcomponents/Sidebar.js
+++ app/dashboardcomponents/Sidebar.js
@@ -1,6 +1,7 @@
 "use client";
 
 import Link from "next/link";
+import Image from "next/image";
 import { usePathname } from "next/navigation";
 import {
   LayoutDashboard,
@@ -40,19 +41,17 @@ export default function Sidebar({ isOpen }) {
 
   return (
     <aside className="fixed left-0 top-0 h-screen w-64 bg-white border-r border-gray-200 shadow-sm z-30">
-      {/* Logo Section */}
-      <div className="p-6 border-b border-gray-200">
-        <div className="flex items-center gap-3">
-          <div className="w-10 h-10 bg-[rgb(46,204,113)] rounded-xl flex items-center justify-center">
-            <Sprout className="w-6 h-6 text-white" />
-          </div>
-          <div>
-            <h1 className="text-gray-900 font-semibold text-lg">
-              FarmConnect
-            </h1>
-            <p className="text-xs text-gray-500">Admin Dashboard</p>
-          </div>
-        </div>
+      {/* Logo Section */} 
+      <div className=" bg-green-700 flex items-center rounded-b-md justify-center h-25">
+            <Image
+              src="/images/logo.png"
+              alt="Logo"
+              width={120}
+              height={60}
+              priority
+              className="object-contain w-auto h-auto"
+            />
+       
       </div>
 
       {/* Menu Section */}
